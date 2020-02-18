d472f6097745d59084902ad884371da36b5940e7
boaTTTTTT <53853565+boaTTTTTT@users.noreply.github.com>
2020-02-18 18:17:49 +0700

    60030089

---
+++ Lab submits/act1/DepartmentStore/DepartmentStore/StoreItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class StoreItem
+{
+    public int itemNumber;
+    public string itemName;
+    public string size;
+    public double unitPrice;
+
+    public void CreateItem()
+    {
+        itemNumber = 9111542;
+        itemName = "CDROM";
+        size = "12";
+        unitPrice = 40;
+    }
+
+    // เพิ่มเมธอดคิดราคาสินค้า
+    public double CalculateTotalValue(int qty)
+    {
+        return unitPrice * qty;
+    }
+
+    public void Describe()
+    {
+        int quantity = 6;
+
+        System.Console.WriteLine("Department Store");
+        System.Console.Write("Item #:      ");
+        System.Console.WriteLine(itemNumber);
+        System.Console.Write("Item Name:   ");
+        System.Console.WriteLine(itemName);
+        System.Console.Write("Item Size:   ");
+        System.Console.WriteLine(size);
+        System.Console.Write("Unit Price:  ");
+        System.Console.WriteLine(unitPrice);
+        System.Console.Write("Quantity:    ");
+        System.Console.WriteLine(quantity);
+        System.Console.Write("Total Value: ");
+        System.Console.WriteLine(CalculateTotalValue(quantity));
+    }
+}
---
+++ Lab submits/act4/Activity4/Activity4/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Rectangle
+{
+    static double w, h;
+    // เมธอดที่ไม่ต้องมีส่วน body
+    public static double calculateArea() => w * h;
+
+    static int Main()
+    {
+        w = 10;
+        h = 20;
+        double area = calculateArea();
+        System.Console.WriteLine("rectangle area = " + area.ToString());
+        System.Console.ReadKey();
+        return 0;
+    }
+}
