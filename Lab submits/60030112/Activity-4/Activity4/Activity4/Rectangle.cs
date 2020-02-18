09de8e1c7995e8a8a0e1f73170332a8aeb9ee30b
AvirutRut <42264185+AvirutRut@users.noreply.github.com>
2020-02-18 16:40:29 +0700

    7

--- Lab submits/60030112/Activity-4/Activity4/Activity4/Rectangle.cs
+++ Lab submits/60030112/Activity-4/Activity4/Activity4/Rectangle.cs
@@ -8,16 +8,19 @@ namespace Activity4
 {
     class Rectangle
     {
+        static double w, h;
         public static double Area(double Width, double Height)
         {
             
             return Width * Height;
         }
-
+        public static double calculateArea() => w * h;
         static int Main()
         {
-            double rectArea = Area(10, 20);
-            System.Console.WriteLine("rectangle area = " + rectArea.ToString());
+            w = 10;
+            h = 20;
+            double area = calculateArea();
+            System.Console.WriteLine("rectangle area = " + area.ToString());
             System.Console.ReadKey();
             return 0;
         }
