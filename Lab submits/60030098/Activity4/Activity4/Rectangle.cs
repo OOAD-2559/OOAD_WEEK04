03aab793c5d229066908603fb6a862f2de5c8d57
iomxin <42264101+iomxin@users.noreply.github.com>
2020-02-18 16:06:14 +0700

    Add imgs Folder in branch master

---
+++ Lab submits/60030098/Activity4/Activity4/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿class Rectangle
+{
+    public static double Area(double Width, double Height)
+    {
+        double result = 0.0;
+        return result;
+    }
+
+    static int Main()
+    {
+        double rectArea = Area(10, 20);
+        System.Console.WriteLine("rectangle area = " + rectArea.ToString());
+        System.Console.ReadKey();
+        return 0;
+    }
+}
