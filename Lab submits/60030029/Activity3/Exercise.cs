b38131319701013ab33e3aeeb8526e0bbe2f0275
nattapad <42264165+nattapad10@users.noreply.github.com>
2020-02-18 16:32:50 +0700

    branch default-parameters
    branch default-parameters

--- Lab submits/60030029/Activity3/Exercise.cs
+++ Lab submits/60030029/Activity3/Exercise.cs
@@ -1,6 +1,6 @@
 ﻿class Exercise
 {
-    void ShowEmployee(int employeeNumber, string fullName, string gender, double hourlySalary)
+    void ShowEmployee(int employeeNumber, string fullName, string gender, double hourlySalary = 500.00D)
     {
         System.Console.WriteLine("Employee Record");
         System.Console.WriteLine("-----------------------------");
@@ -19,6 +19,10 @@
         Exercise exo = new Exercise();
         // เรียกใช้งานโดยส่งพารามิเตอร์ทั้ง 4 ตัวไปให้เมธอด
         exo.ShowEmployee(572948, "Sompong Jaidee", "M", 450.00D);
+        // ละพารามิเตอร์ 1 ตัว จากด้านขวามือสุด
+        exo.ShowEmployee(572948, "Somchai Jaided", "M");
+        // ละพารามิเตอร์ 2 ตัว จากด้านขวามือสุด
+        exo.ShowEmployee(572948, "Sommai Jaisoo");
 
         System.Console.ReadKey();
         return 0;
