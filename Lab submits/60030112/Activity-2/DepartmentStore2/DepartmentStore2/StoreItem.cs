accd977163e40427b8cf8b65b2a780bb26bbf65e
AvirutRut <42264185+AvirutRut@users.noreply.github.com>
2020-02-18 17:03:03 +0700

    2

---
+++ Lab submits/60030112/Activity-2/DepartmentStore2/DepartmentStore2/StoreItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepartmentStore2
+{
+    public class StoreItem
+    {
+        public int itemNumber;
+        public string itemName;
+        public string size;
+        public double unitPrice;
+    }
+}
