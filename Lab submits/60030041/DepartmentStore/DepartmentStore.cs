1f54ae29589a1c7b27fca3fa0f17ac6f17487216
tanakorn128 <42264080+tanakorn128@users.noreply.github.com>
2020-02-18 16:10:00 +0700

    ok

--- Lab submits/60030041/DepartmentStore/DepartmentStore.cs
+++ Lab submits/60030041/DepartmentStore/DepartmentStore.cs
@@ -1,4 +1,10 @@
-﻿public class DepartmentStore
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class DepartmentStore
 {
     static void Main()
     {
--- Lab submits/60030041/DepartmentStore/StoreItem.cs
+++ Lab submits/60030041/DepartmentStore/StoreItem.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class StoreItem
+{
+    public int itemNumber;
+    public string itemName;
+    public string size;
+    public double unitPrice;
+    public void CreateItem()
+    {
+        itemNumber = 792475;
+        itemName = "Wireless Keyboard Logitec 1548";
+        size = "200g.";
+        unitPrice = 1800;
+    }
+
+    public void Describe()
+    {
+        System.Console.WriteLine("Department Store");
+
+        System.Console.Write("Item #:     ");
+        System.Console.WriteLine(itemNumber);
+        System.Console.Write("Item Name:  ");
+        System.Console.WriteLine(itemName);
+        System.Console.Write("Item Size:  ");
+        System.Console.WriteLine(size);
+        System.Console.Write("Unit Price: ");
+        System.Console.WriteLine(unitPrice);
+    }
+}
