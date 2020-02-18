5b2055a8ff47f90ef5f45bc5151c5da2bd55aebb
gingkasina <42264167+gingkasina@users.noreply.github.com>
2020-02-18 16:29:18 +0700

    act2

---
+++ Lab submits/60030007/Activity3/Activity3/Exercise.cs
@@ -0,0 +1,21 @@
+﻿class Exercise
+{
+    ...
+    // ไม่ต้องแก้ไขเมธอด ShowEmployee
+    ...
+
+    static int Main()
+    {
+        Exercise exo = new Exercise();
+
+        // เรียกใช้เมธอดโดยระบุชื่อพารามิเตอร์ทั้ง 4 ตัวตามลำดับโดยระบุชื่อพารามิเตอร์
+        exo.ShowEmployee(fullName: "Somsri Jaiorn", gender: "F", employeeNumber: 84654);
+
+        // เรียกใช้เมธอดโดยระบุชื่อพารามิเตอร์และไม่สนใจลำดับ พร้อมละ default parameter
+        exo.ShowEmployee(fullName: "Somsri Jaiorn", gender: "F", employeeNumber: 84654);
+
+
+        System.Console.ReadKey();
+        return 0;
+    }
+}
