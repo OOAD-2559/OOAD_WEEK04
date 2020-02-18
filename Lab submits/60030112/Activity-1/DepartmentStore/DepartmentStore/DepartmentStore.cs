0aa2cea6470e418c57d4087ebb933b9d564f475f
AvirutRut <42264185+AvirutRut@users.noreply.github.com>
2020-02-18 15:00:15 +0700

    1

---
+++ Lab submits/60030112/Activity-1/DepartmentStore/DepartmentStore/DepartmentStore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepartmentStore
+{
+    public class DepartmentStore
+    {
+        static void Main()
+        {
+            StoreItem si = new StoreItem();
+
+            si.itemNumber = 12081254;
+            si.itemName = "Wireless Mouse Logitec 1542";
+            si.size = "4";
+            si.unitPrice = 1250;
+
+            System.Console.WriteLine("Department Store");
+            System.Console.Write("Item #:     ");
+            System.Console.WriteLine(si.itemNumber);
+            System.Console.Write("Item Name:  ");
+            System.Console.WriteLine(si.itemName);
+            System.Console.Write("Item Size:  ");
+            System.Console.WriteLine(si.size);
+            System.Console.Write("Unit Price: ");
+            System.Console.WriteLine(si.unitPrice);
+
+            System.Console.ReadKey();
+        }
+    }
+}
