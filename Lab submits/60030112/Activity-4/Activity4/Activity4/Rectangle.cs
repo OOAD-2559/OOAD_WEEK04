a3d62fe2c82c3cdda20522454add8da6ea072804
AvirutRut <42264185+AvirutRut@users.noreply.github.com>
2020-02-18 16:26:39 +0700

    2

---
+++ Lab submits/60030112/Activity-4/Activity4/Activity4/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity4
+{
+    class Rectangle
+    {
+        public static double Area(double Width, double Height)
+        {
+            double result = 0.0;
+            return result;
+        }
+
+        static int Main()
+        {
+            double rectArea = Area(10, 20);
+            System.Console.WriteLine("rectangle area = " + rectArea.ToString());
+            System.Console.ReadKey();
+            return 0;
+        }
+    }
+}
