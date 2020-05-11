94e2042046f863f34905b009f420c2f7426bedda
Jarudat Chaikuad <42264164+ucjim@users.noreply.github.com>
2020-05-11 07:04:37 +0700

    T_T
    T_T

---
+++ Lab submits/60030014/Lab-Activity-2/DepartmentStore.cs
@@ -0,0 +1,12 @@
+﻿public class DepartmentStore
+{
+    static void Main()
+    {
+        StoreItem si = new StoreItem();
+
+        si.CreateItem();
+        si.Describe();
+
+        System.Console.ReadKey();
+    }
+}
---
+++ Lab submits/60030014/Lab-Activity-2/StoreItem.cs
@@ -0,0 +1,28 @@
+﻿public class StoreItem
+{
+    public int itemNumber;
+    public string itemName;
+    public string size;
+    public double unitPrice;
+    public void CreateItem()
+    {
+        itemNumber = 792475;
+        itemName = "Wireless Keyboard Logitec 1548";
+        size = "200g.";
+        unitPrice = 1800;
+    }
+
+    public void Describe()
+    {
+        System.Console.WriteLine("Department Store");
+
+        System.Console.Write("Item #:     ");
+        System.Console.WriteLine(itemNumber);
+        System.Console.Write("Item Name:  ");
+        System.Console.WriteLine(itemName);
+        System.Console.Write("Item Size:  ");
+        System.Console.WriteLine(size);
+        System.Console.Write("Unit Price: ");
+        System.Console.WriteLine(unitPrice);
+    }
+}
